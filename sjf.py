1aadbad074cb2d7bcb9d4709b8cb1fbf8d3bc8b3
Shaheed-Shawon <136836283+Shaheed-Shawon@users.noreply.github.com>
2025-07-25 22:17:40 +0600

    Update sjf.py

--- sjf.py
+++ sjf.py
@@ -1,39 +1,51 @@
 import numpy
-process={
-    0:5,
-    1:2,
-    2:1,
-    3:4
+
+
+process = {
+    0: [5, 2],
+    1: [2],
+    2: [1],
+    3: [4]
 }
-arrival=[0,1,2,3]
-burst=[process[i] for i in range(len(process))]
-n=len(process)
-ct=[0]*n
-tat=[0]*n
-wt=[0]*n
-initially=0
-done=[0]*n
+
+
+arrival = []
+burst = []
+pid = 0
+for key in sorted(process.keys()):
+    for b in process[key]:
+        arrival.append(key)
+        burst.append(b)
+        pid += 1
+
+n = len(arrival)
+ct = [0]*n
+tat = [0]*n
+wt = [0]*n
+initially = 0
+done = [0]*n
 
 for _ in range(n):
-    idx=-1
-    shortest=9999
+    idx = -1
+    shortest = 9999
     for i in range(n):
-        if arrival[i]<=initially and not done[i] and burst[i]<shortest:
-            shortest=burst[i]
-            idx=i
-    if idx==-1:
-        initially+=1
+        if arrival[i] <= initially and not done[i] and burst[i] < shortest:
+            shortest = burst[i]
+            idx = i
+    if idx == -1:
+        initially += 1
         continue
-    ct[idx]=initially+burst[idx]
-    tat[idx]=ct[idx]-arrival[idx]
-    wt[idx]=tat[idx]-burst[idx]
-    initially=ct[idx]
-    done[idx]=1
+    ct[idx] = initially + burst[idx]
+    tat[idx] = ct[idx] - arrival[idx]
+    wt[idx] = tat[idx] - burst[idx]
+    initially = ct[idx]
+    done[idx] = 1
 
-print(ct)
-print(tat)
-print(wt)
+print("CT:", ct)
+print("TAT:", tat)
+print("WT:", wt)
+
 avg_tat = sum(tat) / n
-avg_wt  = sum(wt)  / n
-print(avg_tat)
-print(avg_wt)
+avg_wt = sum(wt) / n
+print("Average TAT:", avg_tat)
+print("Average WT:", avg_wt)
