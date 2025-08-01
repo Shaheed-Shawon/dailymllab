b24037259793f1fab1f3d1fee9ee67c3405bd6c0
Shaheed-Shawon <136836283+Shaheed-Shawon@users.noreply.github.com>
2025-08-01 16:58:27 +0600

    Create round robin.py

---
+++ round robin.py
@@ -0,0 +1,35 @@
+pid = [1, 2, 3]
+at = [0, 1, 2]
+bt = [10, 4, 5]
+q = 2
+n = len(pid)
+rt = bt[:]
+ct = [0] * n
+t = 0
+done = 0
+rq = []
+
+while done < n:
+    for i in range(n):
+        if at[i] <= t and rt[i] > 0 and i not in rq:
+            rq.append(i)
+    if rq:
+        i = rq.pop(0)
+        run = min(q, rt[i])
+        t += run
+        rt[i] -= run
+        if rt[i] == 0:
+            ct[i] = t
+            done += 1
+        else:
+            rq.append(i)
+    else:
+        t += 1
+
+tat = [ct[i] - at[i] for i in range(n)]
+wt = [tat[i] - bt[i] for i in range(n)]
+
+print("CT:", ct)
+print("TAT:", tat)
+print("WT:", wt)
+print("Avg CT:", sum(ct) / n)
